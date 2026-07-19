b93ddc7472c001ca59e1300ceeb820ad0756bf11
barna-tudor <tudorbarna00@gmail.com>
2026-07-20 00:23:07 +0300

    add db connection

--- Presentation.Api/Program.cs
+++ Presentation.Api/Program.cs
@@ -1,9 +1,21 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContextFactory<DataBaseContext>(options =>
+{
+    if (connectionString != null)
+        options.UseMySQL(connectionString);
+    else
+        throw new Exception("Missing ConnectionString in appsettings.[Development.]json");
+});
 
 var app = builder.Build();
 
