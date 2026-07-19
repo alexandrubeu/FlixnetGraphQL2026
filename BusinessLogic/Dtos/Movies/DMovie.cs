73cc1f65d0c54e61e7a31a5cc8bdd7a65bf9e2d8
barna-tudor <tudorbarna00@gmail.com>
2026-07-19 14:55:55 +0300

    [Housekeeping] missed namespace scope->file

--- BusinessLogic/Dtos/Movies/DMovie.cs
+++ BusinessLogic/Dtos/Movies/DMovie.cs
@@ -1,17 +1,16 @@
 using BusinessLogic.Dtos.CastCrew;
 using BusinessLogic.Dtos.Genres;
 
-namespace BusinessLogic.Dtos.Movies
+namespace BusinessLogic.Dtos.Movies;
+
+public class DMovie
 {
-    public class DMovie
-    {
-        public int Id { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string ImageUrl { get; set; } = string.Empty;
-        public string VideoSource { get; set; } = string.Empty;
-        public string? TrailerUrl { get; set; }
-        public bool Published { get; set; }
-        public List<DGenre> Genres { get; set; } = new();
-        public List<DCastCrewCredit> CastAndCrew { get; set; } = new();
-    }
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string ImageUrl { get; set; } = string.Empty;
+    public string VideoSource { get; set; } = string.Empty;
+    public string? TrailerUrl { get; set; }
+    public bool Published { get; set; }
+    public List<DGenre> Genres { get; set; } = new();
+    public List<DCastCrewCredit> CastAndCrew { get; set; } = new();
 }
