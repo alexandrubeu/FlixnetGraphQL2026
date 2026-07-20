f22144f5691bc527815c7813e4057133dc58dfd4
AndreiBcs <andreibichis1@gmail.com>
2026-07-20 21:23:44 +0300

    queries + mutations on mock db

--- BusinessLogic/Dtos/Collections/DCollection.cs
+++ BusinessLogic/Dtos/Collections/DCollection.cs
@@ -2,6 +2,12 @@ namespace BusinessLogic.Dtos
 {
     public class DCollection
     {
+        public DCollection(int id, bool published)
+        {
+            Id = id;
+            Published = published;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public bool Published { get; set; }
--- BusinessLogic/Dtos/Genres/DGenre.cs
+++ BusinessLogic/Dtos/Genres/DGenre.cs
@@ -2,6 +2,12 @@ namespace BusinessLogic.Dtos
 {
     public class DGenre
     {
+        public DGenre(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
     }
