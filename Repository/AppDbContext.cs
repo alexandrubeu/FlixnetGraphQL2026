aa2613e9507bf7102448cdcb0db1485a3ec541c4
Florin-David Coman <imortalgalaxy1018@gmail.com>
2026-07-21 15:53:37 +0300

    feat: repository implementations (#19)
    * feat: added entity classes for DTOs
    
    * fix: nullable Movie navigation property, replace trailers collection with single URL
    
    * [chore] added dependencies and .gitignore
    
    * added AppDbContext
    
    * feat: implement repository classes
    
    * add
    
    * feat: created migrations and GenresServices
    
    ---------
    
    Co-authored-by: Darius <olteidarius21@gmail.com>

--- Repository/AppDbContext.cs
+++ Repository/AppDbContext.cs
@@ -5,7 +5,8 @@ namespace Repository
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
 
         public DbSet<EMovie> Movies { get; set; }
         public DbSet<EGenre> Genres { get; set; }
@@ -14,7 +15,11 @@ namespace Repository
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnConfiguring(optionsBuilder.UseMySQL("server=localhost;port=3306;database=flixnet;user=root;password=7410;"));
+            base.OnConfiguring(
+                optionsBuilder.UseMySQL(
+                    "server=localhost;port=3306;database=flixnet;user=root;password=7410;"
+                )
+            );
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -29,15 +34,12 @@ namespace Repository
             modelBuilder.Entity<ECollection>().Property(e => e.Id).IsRequired();
             modelBuilder.Entity<ECastCrewCredit>().Property(e => e.Id).IsRequired();
 
-            modelBuilder.Entity<EMovie>()
-                .HasMany(m => m.Genres)
-                .WithMany(g => g.Movies);
+            modelBuilder.Entity<EMovie>().HasMany(m => m.Genres).WithMany(g => g.Movies);
 
-            modelBuilder.Entity<EMovie>()
-                .HasMany(m => m.Collections)
-                .WithMany(c => c.Movies);
+            modelBuilder.Entity<EMovie>().HasMany(m => m.Collections).WithMany(c => c.Movies);
 
-            modelBuilder.Entity<ECastCrewCredit>()
+            modelBuilder
+                .Entity<ECastCrewCredit>()
                 .HasOne(c => c.Movie)
                 .WithMany(m => m.CastAndCrew)
                 .HasForeignKey(c => c.MovieId);
