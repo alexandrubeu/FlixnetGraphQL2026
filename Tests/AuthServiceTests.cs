e62b94e50d47ad1ae889aad4841031266623e486
barna-tudor <tudorbarna00@gmail.com>
2026-07-23 12:31:20 +0300

    [Test] /register fails when supplied with existing username

--- Tests/AuthServiceTests.cs
+++ Tests/AuthServiceTests.cs
@@ -12,16 +12,28 @@ public class AuthServiceTests
     public async Task LoginAsync_ThrowsWhenUserNotFound()
     {
         var userManager = new Mock<UserManager<EUser>>(
-            Mock.Of<IUserStore<EUser>>(), 
-            null, null, null, null, null, null, null, null); // only store is needed
-        
+            Mock.Of<IUserStore<EUser>>(), null, null, null, null, null, null, null, null); // only store is needed
+
         var tokenService = new Mock<ITokenService>();
         var authService = new AuthService(userManager.Object, tokenService.Object);
 
-        await Assert.ThrowsAsync<GraphQLException>(
-            () => authService.LoginAsync(new LoginInput("nonexistent", "password"))
+        await Assert.ThrowsAsync<GraphQLException>(() =>
+            authService.LoginAsync(new LoginInput("nonexistent", "password"))
         );
-        
         // use 'dotnet test Tests'
+    }
+
+    [Fact]
+    public async Task registerAsync_ThrowsWhenUserExists()
+    {
+        var userManager = new Mock<UserManager<EUser>>(
+            Mock.Of<IUserStore<EUser>>(), null, null, null, null, null, null, null, null);
+        var tokenService = new Mock<ITokenService>();
+        var authService = new AuthService(userManager.Object, tokenService.Object);
+
+        userManager.Setup(umProxy => umProxy.FindByNameAsync(It.IsAny<string>())).ReturnsAsync(new EUser());
+
+        await Assert.ThrowsAsync<GraphQLException>(() =>
+            authService.RegisterAsync(new LoginInput("username", "password")));
     }
 }
