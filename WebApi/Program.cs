c9dccc5a9788e308a42916b785c58276d95a3729
barna-tudor <tudorbarna00@gmail.com>
2026-07-23 14:47:09 +0300

    move from .env to appsettings.json

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,19 +1,16 @@
 using System.Text;
 using BusinessLogic;
 using BusinessLogic.Auth;
-using DotNetEnv;
 using Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Repository;
 using WebApi.Auth;
 using WebApi.GraphQL.Mutations;
 using WebApi.GraphQL.Queries;
 
-Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -34,12 +31,10 @@ builder
     .AddFiltering()
     .AddSorting()
     .AddProjections()
-    .ModifyRequestOptions(o =>
-    {
-        o.IncludeExceptionDetails = true;
-    });
+    .ModifyRequestOptions(o => { o.IncludeExceptionDetails = true; });
 
 
+/* OLD
 // JWT Configuration
 var jwt = new JwtSettings
 {
@@ -50,8 +45,12 @@ var jwt = new JwtSettings
     Audience = Environment.GetEnvironmentVariable("JWT_AUDIENCE") ?? "MovieAPI",
     ExpiryMinutes = int.Parse(Environment.GetEnvironmentVariable("JWT_EXPIRY_MINUTES") ?? "60"),
 };
-
 builder.Services.AddSingleton(Options.Create(jwt));
+*/
+
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("Jwt"));
+var jwt = builder.Configuration.GetSection("Jwt").Get<JwtSettings>()
+          ?? throw new Exception("[AUTH][JWT] appsettings.json is missing the Jwt section. ");
 
 builder
     .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
